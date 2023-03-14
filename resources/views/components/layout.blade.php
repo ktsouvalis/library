239a45ddd1fa1ddc98c460fdee375c308ae4273a
Konstantinos Stefanopoulos <konstantinostef@yahoo.gr>
2023-03-14 14:30:53 +0200

    complete index.blade.php css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Βιβιλιοθήκη</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
 
     <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -28,5 +28,5 @@
       $('[data-toggle="tooltip"]').tooltip()
     </script>
     </div> <!-- container closing -->
-  </body>
+   </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,64 +1,83 @@
+
+
 <x-layout>
-<body>
-<div class="container bg-light xxl">
-    <p class="h3">Βιβλιοθήκη 45ου Δημοτικού Σχολείου Πάτρας</p>
-    <div class="d-flex flex row p-2 justify-content-around">
-    <div><h2><a href="/show_all_books" target="_blank">Κατάλογος Βιβλίων</a></h2></div>
-    @auth
-        <a href='/logout'>Αποσύνδεση</a>
-        <br><br><br>
-        {{-- <h2><a href="#" target="_blank">Εισαγωγή βιβλίων από αρχείο</a></h2>
-        <h2><a href="#" target="_blank">Εισαγωγή βιβλίων μέσω φόρμας</a></h2> --}}
-        <br><br>
-        <h2><a href="#" target="_blank">Αναζήτηση Βιβλίου</a></h2>
-        <h2><a href="/search_student">Αναζήτηση Μαθητή με Α.Μ.</a></h2>
-        <br><br>
-        {{-- <h2><a href="/show_who_books_out" target="_blank">Αναζήτηση δανεισμών ανά τμήμα</a></h2>
-        <h2><a href="/number_of_total_outs_per_class" target="_blank">Αριθμός συνολικών δανεισμών ανά τάξη</a></h2>
-        <h2><a href="/all_books_to_xlsx" target="_blank">Εξαγωγή Βάσης σε αρχείο xlsx</a></h2> --}}
-        <br><br>
-        {{-- <h2><a href="#" target="_blank">Επεξεργασία βιβλίου</a></h2> --}}
-    @else
-        <form action="/login" method="post">
-            @csrf
-            <div class="mb-3">
-                <label for="name" class="form-label">Όνομα Χρήστη</label>
-                <div class="">
-                    <input type="text" value="{{old('name')}}" name="name" class="form-control">
-                    @error('name')
-                        {{$message}}
-                    @enderror
-                </div>
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Κωδικός</label>
-                <div class="">
-                    <input type="password" name="password" class="form-control">
-                    @error('password')
-                        {{$message}}
-                    @enderror
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Είσοδος</button>
-        </form>
-
-    @endauth
-    </div>
-
-    @if (session()->has('success'))
-    <div class='container container--narrow'>
-      <div class='alert alert-success text-center'>
-        {{session('success')}}
-      </div>
-    </div>
-    @endif
-
-    @if(session()->has('failure'))
-    <div class='container container--narrow'>
-    <div class='alert alert-danger text-center'>
-        {{session('failure')}}
-    </div>
-    </div>
-    @endif    
+    <body class="bg-light">
+  
+      <br><br><br><br><br><br>
+    <div class="container">
+       
+        <p class="h3">Βιβλιοθήκη 45ου Δημοτικού Σχολείου Πάτρας</p>
     
+        
+        <div class="row p-2 justify-content-evenly">
+            <a class="col-lg-3 card text-bg-secondary mb-3" style="max-width: 18rem; opacity: 0.7;" href="#">
+           
+                
+                <div class="card-body" style="text-align: center; padding: 5rem">
+                  <h5 class="card-title">Κατάλογος Βιβλίων</h5>
+                  <p class="card-text"></p>
+                </div>
+            
+            </a>
+        @auth
+            <a href='/logout'>Αποσύνδεση</a>
+            <br><br><br>
+            {{-- <h2><a href="#" target="_blank">Εισαγωγή βιβλίων από αρχείο</a></h2>
+            <h2><a href="#" target="_blank">Εισαγωγή βιβλίων μέσω φόρμας</a></h2> --}}
+            <br><br>
+            <h2><a href="#" target="_blank">Αναζήτηση Βιβλίου</a></h2>
+            <h2><a href="/search_student">Αναζήτηση Μαθητή με Α.Μ.</a></h2>
+            <br><br>
+            {{-- <h2><a href="/show_who_books_out" target="_blank">Αναζήτηση δανεισμών ανά τμήμα</a></h2>
+            <h2><a href="/number_of_total_outs_per_class" target="_blank">Αριθμός συνολικών δανεισμών ανά τάξη</a></h2>
+            <h2><a href="/all_books_to_xlsx" target="_blank">Εξαγωγή Βάσης σε αρχείο xlsx</a></h2> --}}
+            <br><br>
+            {{-- <h2><a href="#" target="_blank">Επεξεργασία βιβλίου</a></h2> --}}
+        @else
+    
+        <div class="col-lg-3 p-3">
+            <form action="/login" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Όνομα Χρήστη</label>
+                    <div class="">
+                        <input type="text" value="{{old('name')}}" name="name" class="form-control">
+                        @error('name')
+                            {{$message}}
+                        @enderror
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Κωδικός</label>
+                    <div class="">
+                        <input type="password" name="password" class="form-control">
+                        @error('password')
+                            {{$message}}
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Είσοδος</button>
+            </form>
+        </div>
+    
+    
+        @endauth
+        
+    
+        @if (session()->has('success'))
+        <div class='container container--narrow'>
+          <div class='alert alert-success text-center'>
+            {{session('success')}}
+          </div>
+        </div>
+        @endif
+    
+        @if(session()->has('failure'))
+        <div class='container container--narrow'>
+        <div class='alert alert-danger text-center'>
+            {{session('failure')}}
+        </div>
+        </div>
+        @endif    
+        
 </x-layout>
