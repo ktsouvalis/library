e563ac0e03d96ce92e814fe841aaa8c5a63ae13a
ktsouvalis <ktsouvalis@sch.gr>
2023-03-23 18:32:57 +0200

    fix alert book cannot be lended

--- resources/views/book-profile.blade.php
+++ resources/views/book-profile.blade.php
@@ -13,13 +13,8 @@
             <a href="">Καταχώρηση δανεισμού</a>
         </div>
     @else
-        <div class="toast align-items-center text-white bg-danger border-0" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="d-flex">
-                <div class="toast-body">
-                    Το βιβλίο δεν είναι διαθέσιμο για δανεισμό
-                </div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
+        <div class="alert alert-danger fade show" role="alert">
+            <strong>Το βιβλίο δεν είναι διαθέσιμο για δανεισμό</strong>
         </div>
     @endif
     <br><br><br>
