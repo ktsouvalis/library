907916a4e06aa06a0d58607354972154b2fc11ae
Konstantinos Tsouvalis <ktsouvalis@sch.gr>
2023-04-04 14:56:00 +0300

    remove /all-books from index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,12 +38,12 @@
                 </div> 
             </a>
         @else
-        <a class="col-lg-3 card text-bg-primary mb-3" style="max-width: 20rem; opacity: 0.9;" href="/all-books">
+        {{-- <a class="col-lg-3 card text-bg-primary mb-3" style="max-width: 20rem; opacity: 0.9;" href="/all-books">
                 <div class="card-body" style="text-align: center; padding: 5rem">
                   <h5 class="card-title">Κατάλογος Βιβλίων</h5>
                   <p class="card-text"></p>
                 </div> 
-        </a>
+        </a> --}}
         <div class="col-lg-3 p-3">
             <form action="/login" method="post">
                 @csrf
