0599f8995ca2df9711b6da423a3d9332344e4f0c
ktsouvalis <ktsouvalis@sch.gr>
2023-07-28 10:47:05 +0300

    implement exclude/include search to datatable initialization

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -85,36 +85,74 @@
 
     <script>
 $(document).ready(function () {
-  // Setup - add a text input to each header cell
-  $('#dataTable thead tr #search').each(function () {
-    var title = $(this).text();
-    $(this).html('<input type="text" style="width:7rem;" placeholder="' + title + '" />');
-  });
+    // Setup - add a text input for inclusion and exclusion to each header cell
+    $('#dataTable thead tr #search').each(function () {
+        var title = $(this).text();
+        $(this).html(`
+            <div class="vstack gap-1">
+                <input type="text" class="include-search" style=" font-size:small;" placeholder="${title} +" />
+                <input type="text" class="exclude-search" style=" font-size:small;" placeholder="${title} - " />
+            </div>
+        `);
+    });
 
-  // DataTable
-  var table = $('#dataTable').DataTable({
-    initComplete: function () {
+    // DataTable
+    var table = $('#dataTable').DataTable({
+        lengthMenu: [10, 25, 50, 100, -1], // Add -1 for "All"
+        pageLength: 10, // Set the initial page length
+        initComplete: function () {
+            // Apply the search
+            this.api().columns().every(function () {
+                var that = this;
+                var includeColumn = $('input.include-search', this.header());
+                var excludeColumn = $('input.exclude-search', this.header());
 
-      // Apply the search
-      this.api()
-        .columns()
-        .every(function () {
-          var that = this;
-          var column = this;
+                includeColumn.on('keyup change clear', function () {
+                    var includeValue = this.value;
+                    var excludeValue = excludeColumn.val();
+                    var regex;
 
-          $('input', this.header()).on('keyup change clear', function () {
-            if (that.search() !== this.value) {
-              that.search(this.value).draw();
-            }
-          }).on('click', function(e) {
-            e.stopPropagation(); // Stop the click event from propagating to the DataTables header cell
-            // table.ordering([[], []]); // Toggle sorting off
-            column.search($(this).val()).draw(); // Apply the search filter
-          });
-        });
-    },
-  });
+                    if (includeValue) {
+                        if (excludeValue) {
+                            regex = `^(?=.*${includeValue})(?!.*${excludeValue})`;
+                        } else {
+                            regex = `.*${includeValue}`;
+                        }
+                    } else {
+                        regex = excludeValue ? `^(?!.*${excludeValue}).*` : '';
+                    }
+
+                    that.search(regex, true, false).draw();
+                }).on('click', function (e) {
+                    e.stopPropagation();
+                    column.search($(this).val()).draw();
+                });
+
+                excludeColumn.on('keyup change clear', function () {
+                    var excludeValue = this.value;
+                    var includeValue = includeColumn.val();
+                    var regex;
+
+                    if (excludeValue) {
+                        if (includeValue) {
+                            regex = `^(?=.*${includeValue})(?!.*${excludeValue})`;
+                        } else {
+                            regex = `^(?!.*${excludeValue}).*`;
+                        }
+                    } else {
+                        regex = includeValue ? `.*${includeValue}` : '';
+                    }
+
+                    that.search(regex, true, false).draw();
+                }).on('click', function (e) {
+                    e.stopPropagation();
+                    column.search($(this).val()).draw();
+                });
+            });
+        },
+    });
 });
+
 </script>
 
     
